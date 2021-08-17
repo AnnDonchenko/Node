be82e62d2bf4794eb23bf5a146622a8ff47b005b
novik <annie5donchenko@gmail.com>
2021-08-17 16:01:31 +0300

    second task

--- app.js
+++ app.js
@@ -6,36 +6,48 @@ const path = require('path');
 // Oleg.txt в якому міститься {"gender": "male"}
 // Вам потрібно перемістити всіх дівчат на 1800 а хлопців на 2000.
 
-console.log('-------------1--------------');
-const filesPath1800 = path.join(__dirname, '1800');
-const filesPath2000 = path.join(__dirname, '2000');
-
-moveFiles(filesPath1800, filesPath2000, "male");
-moveFiles(filesPath2000, filesPath1800, "female");
-
-function moveFiles(oldPath, newPath, condition){
-    fs.readdir(oldPath, (err, files) => {
-        if (err) throw err;
-
-        files.forEach(file => {
-            const oldFilePath = path.join(oldPath, file);
-            const newFilePath = path.join(newPath, file);
-
-            fs.readFile(oldFilePath, (err, data) => {
-                if (err) throw err;
-                const obj = JSON.parse(data.toString());
-                if (obj.gender === condition) {
-                    fs.rename(oldFilePath, newFilePath, (err) => {
-                        if (err) throw err;
-                        console.log(obj.name, obj.gender, 'Rename complete!');
-                    })
-                }
-            });
-        })
-    });
-}
+// console.log('-------------1--------------');
+// const filesPath1800 = path.join(__dirname, '1800');
+// const filesPath2000 = path.join(__dirname, '2000');
+//
+// moveFiles(filesPath1800, filesPath2000, "male");
+// moveFiles(filesPath2000, filesPath1800, "female");
+//
+// function moveFiles(oldPath, newPath, condition){
+//     fs.readdir(oldPath, (err, files) => {
+//         if (err) throw err;
+//
+//         files.forEach(file => {
+//             const oldFilePath = path.join(oldPath, file);
+//             const newFilePath = path.join(newPath, file);
+//
+//             fs.readFile(oldFilePath, (err, data) => {
+//                 if (err) throw err;
+//                 const obj = JSON.parse(data.toString());
+//                 if (obj.gender === condition) {
+//                     fs.rename(oldFilePath, newFilePath, (err) => {
+//                         if (err) throw err;
+//                         console.log(obj.name, obj.gender, 'Rename complete!');
+//                     });
+//                 }
+//             });
+//         });
+//     });
+// }
 
 // * вам потрбіно перемісти всі файлики з вкладених папок в іншу папку.
 // Зробити всі файли на одному рівні вкладеності.
 // (Більше інформації в записі лекції)
 
+console.log('-------------2--------------');
+function getFilesFromFolders(){
+    const folderOutPath = path.join(__dirname, 'folder_1lvl');
+    fs.readdir(folderOutPath, (err, files) => {
+        if (err) throw err;
+
+        files.forEach(file => {
+            console.log(file);
+        });
+    });
+}
+getFilesFromFolders();
