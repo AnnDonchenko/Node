f27466ad7c193148a75d4628138ba61d56883948
novik <annie5donchenko@gmail.com>
2021-08-19 14:57:01 +0300

    create files

--- app.js
+++ app.js
@@ -1,1 +1,21 @@
-console.log('HELLO')
+const express = require('express');
+const expressHbs = require('express-handlebars')
+const path = require('path');
+
+const users = require('./db/users');
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+
+app.use(express.static(path.join(__dirname, 'static')));
+app.set('view engine', '.hbs');
+app.engine('.hbs', expressHbs({defaultLayout: false}));
+app.set('views', path.join(__dirname, 'static'));
+
+app.get('/ping', (req, res)=>{
+    res.json('Pong');
+});
+
+console.log(users);
---
+++ db/users.js
@@ -0,0 +1,6 @@
+module.exports = [
+    { name: 'Dima', age: 22, password: 'Pa$$w0rd' },
+    { name: 'Vika', age: 18, password: 'Pa$$w0rd' },
+    { name: 'Khrystyna', age: 16, password: 'Pa$$w0rd' },
+    { name: 'Viktor', age: 25, password: 'Pa$$w0rd' },
+]
