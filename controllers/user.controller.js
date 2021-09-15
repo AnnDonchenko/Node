73557bcd857731fb0cb4b05bbe7b0b40283fe86a
novik <annie5donchenko@gmail.com>
2021-09-15 14:21:15 +0300

    delete tokens in res

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -63,10 +63,7 @@ module.exports = {
                 }
             );
 
-            res.status(statusCodes.created).json({
-                ...token,
-                user: userToReturn
-            });
+            res.status(statusCodes.created).json(userToReturn);
         } catch (e) {
             next(e);
         }
@@ -102,10 +99,7 @@ module.exports = {
                 }
             );
 
-            res.status(statusCodes.created).json({
-                ...token,
-                user: userToReturn
-            });
+            res.status(statusCodes.created).json(userToReturn);
         } catch (e) {
             next(e);
         }
